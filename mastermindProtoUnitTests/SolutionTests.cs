db14a1ca6ae62bc22fad95999820faa4a7bfb4b4
Michael Rybintsev <mrybintsev@neworbit.co.uk>
2017-12-29 16:18:40 +0000

    added peg tests

--- mastermindProtoUnitTests/SolutionTests.cs
+++ mastermindProtoUnitTests/SolutionTests.cs
@@ -2,6 +2,7 @@ using System;
 using Shouldly;
 using Xunit;
 using mastermindProto;
+using Xunit.Sdk;
 
 namespace mastermindProtoUnitTests
 {
@@ -11,7 +12,29 @@ namespace mastermindProtoUnitTests
         public void CanCreatePermutationSet()
         {
             var set = FiveStepAlgorithm.CreatePermutationSet();
-            Assert.Equal(1296, set.Count);
+
+            var expected = 1296;
+            var actual = set.Count;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetPegsForReturnsStringFourCharactersLong()
+        {
+           Assert.IsType<string>(FiveStepAlgorithm.GetPegsFor(1122, 1234));
+        }
+
+        [Fact]
+        public void GetsPegsForGuesses()
+        {
+            Assert.Equal(FiveStepAlgorithm.GetPegsFor(1122, 1234), "bwnn");
+            Assert.Equal(FiveStepAlgorithm.GetPegsFor(2211, 1234), "wbnn");
+            Assert.Equal(FiveStepAlgorithm.GetPegsFor(1234, 4321), "wwww");
+            Assert.Equal(FiveStepAlgorithm.GetPegsFor(3333, 1111), "nnnn");
+            Assert.Equal(FiveStepAlgorithm.GetPegsFor(1111, 1111), "bbbb");
+            Assert.Equal(FiveStepAlgorithm.GetPegsFor(1233, 3312), "wwwn"); // would this be right though? Think so...
+            Assert.Equal(FiveStepAlgorithm.GetPegsFor(4421, 4413), "bbwn");
         }
     }
 }
