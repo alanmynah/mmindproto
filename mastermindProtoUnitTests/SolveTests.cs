75a9f6bc3d5d1f5247bc115779d9f620c6b40bb9
Michael Rybintsev <mrybintsev@neworbit.co.uk>
2018-01-03 10:08:01 +0000

    small refactoring, solution tests fixed ncrunch

---
+++ mastermindProtoUnitTests/SolveTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using mastermindProto;
+using Xunit;
+
+namespace mastermindUnitTests
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void FindsSolutionsForVariousCombinations()
+        {
+            var comboSet = FiveStepAlgorithm.CreatePermutationSet();
+            var solutionSet = new List<int>() { };
+
+            foreach (var possibleCombination in comboSet)
+            {
+                Solution solution = new Solution();
+                solution.PrintSolution(possibleCombination);
+                solutionSet.Add(solution.GuessList[solution.GuessList.Count - 1]);
+            }
+
+            Assert.Equal(comboSet, solutionSet);
+        }
+    }
+}
